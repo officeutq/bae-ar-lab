fcd20317b687753f3deb1a7b5f3a29cfd84d7349
officeutq <245330306+officeutq@users.noreply.github.com>
2026-05-15 14:34:10 +0900

    Update vite config to include mkcert plugin hook

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+import type { PluginOption } from 'vite';
+
+const mkcert = (): PluginOption => ({
+  name: 'mkcert-local-plugin',
+});
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     host: true,
     https: {},
